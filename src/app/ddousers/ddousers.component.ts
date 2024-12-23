de2c10d47138820af360c02be31e23c8fdc05507
Kiran1205 <kiranvarma149@gmail.com>
2024-12-23 21:01:12 +0530

    updaetd ddo users

--- src/app/ddousers/ddousers.component.ts
+++ src/app/ddousers/ddousers.component.ts
@@ -1,11 +1,85 @@
-import { Component } from '@angular/core';
+
+import { Component, TemplateRef} from '@angular/core';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import {SelectionModel} from '@angular/cdk/collections';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';  // Optional, for icon buttons
+import { MatTooltipModule } from '@angular/material/tooltip';
+
+import {MatDividerModule} from '@angular/material/divider';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-ddousers',
-  imports: [],
+  imports: [MatTableModule, MatCheckboxModule,MatButtonModule,
+    MatIconModule,
+    MatTooltipModule,
+    MatDividerModule,
+    MatDialogModule,
+        MatInputModule,
+        ReactiveFormsModule],
   templateUrl: './ddousers.component.html',
   styleUrl: './ddousers.component.css'
 })
-export class DdousersComponent {
+export class DdousersComponent  {
+
+  displayedColumns: string[] = [ 'position', 'name', 'kgid', 'phno','location','station','actions'];
+  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+  ddouserForm: FormGroup;
+
+  constructor(private fb: FormBuilder,public dialog: MatDialog) {
+      // Initialize the form
+      this.ddouserForm = this.fb.group({
+        name: ['', Validators.required],
+        kgid: ['', [Validators.required]],
+        location:[''],
+        station:[''],
+        phno:[]
+      });
+    }
+
+  // Open the "Add Student" dialog
+   openAddDialog(dialogTemplateRef : TemplateRef<any>): void {
+      const dialogRef = this.dialog.open(dialogTemplateRef, {
+        width: '500px'
+      });
+
+      dialogRef.afterClosed().subscribe(result => {
+        if (result) {
+         // this.students.push(result); // Add the new student to the list
+        }
+      });
+    }
+
+
+  editRecord(): void {
+    console.log('edit:');
+  }
 
 }
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  kgid: number;
+  location: string;
+  phno: number;
+  station: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Kiran', kgid: 1234567, location: 'West banglore',phno:8884684500,station:'Madiwala'},
+  {position: 2, name: 'Dileep', kgid: 1234567, location: 'West banglore',phno:8884684500,station:'Madiwala'},
+  {position: 3, name: 'Thulasi', kgid: 1234567, location: 'West banglore',phno:8884684500,station:'Madiwala'},
+  {position: 4, name: 'Pradeep', kgid: 1234567, location: 'West banglore',phno:8884684500,station:'Madiwala'},
+  {position: 1, name: 'Kiran', kgid: 1234567, location: 'West banglore',phno:8884684500,station:'Madiwala'},
+  {position: 2, name: 'Dileep', kgid: 1234567, location: 'West banglore',phno:8884684500,station:'Madiwala'},
+  {position: 3, name: 'Thulasi', kgid: 1234567, location: 'West banglore',phno:8884684500,station:'Madiwala'},
+  {position: 4, name: 'Pradeep', kgid: 1234567, location: 'West banglore',phno:8884684500,station:'Madiwala'},
+  {position: 1, name: 'Kiran', kgid: 1234567, location: 'West banglore',phno:8884684500,station:'Madiwala'},
+  {position: 2, name: 'Dileep', kgid: 1234567, location: 'West banglore',phno:8884684500,station:'Madiwala'},
+  {position: 3, name: 'Thulasi', kgid: 1234567, location: 'West banglore',phno:8884684500,station:'Madiwala'},
+  {position: 4, name: 'Pradeep', kgid: 1234567, location: 'West banglore',phno:8884684500,station:'Madiwala'},
+];
